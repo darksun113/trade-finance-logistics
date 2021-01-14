5c6b4473453730a37ad2f79c220b3b74274f184b
darksun113 <mtion@msn.com>
2021-01-14 15:42:32 +0800

    customer user login

--- application/client_scripts/userLogin.sh
+++ application/client_scripts/userLogin.sh
@@ -1,13 +1,15 @@
-if [ $# -eq 3 ]
+if [ $# -eq 2 ]
 then
-    echo "Using customized arguments"
-fi
+        echo "Processing customized arguments."
+        curl -s -X POST http://localhost:4000/login -H "content-type: application/x-www-form-urlencoded" -d "username=$1&orgName=$2" > userCred.json
+else
 
-curl -s -X POST http://localhost:4000/login -H "content-type: application/x-www-form-urlencoded" -d 'username=Jim&orgName=importerorg' > userCred.json
+        curl -s -X POST http://localhost:4000/login -H "content-type: application/x-www-form-urlencoded" -d 'username=Jim&orgName=importerorg' > userCred.json
+fi
 JWT=$(jq '.token' userCred.json)
 if [ $JWT == null ]
 then
-	echo "Failed to login user Jim"
+        echo "Failed to login user Jim"
 else
-	echo "Successfully obtained user Jim login credentials. See 'userCred.json'."
+        echo "Successfully obtained user Jim login credentials. See 'userCred.json'."
 fi
