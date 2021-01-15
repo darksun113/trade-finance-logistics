527a33af98f38752d3a24c9870f0056a66b7614c
darksun113 <mtion@msn.com>
2021-01-15 11:32:51 +0800

    payment

--- application/client_scripts/query_getAccountBalance.sh
+++ application/client_scripts/query_getAccountBalance.sh
@@ -14,10 +14,8 @@ if [ ${JWT:$JLEN:1} == "\"" ]
 then
 	JWT=${JWT:0:$JLEN}
 fi
-CC=$(curl -s -X GET http://localhost:4000/chaincode/getAccountBalance -H "content-type: application/json" -H "authorization: Bearer $JWT" -d '{ "ccversion": "v0", "args": ["2ks89j9","${entity}"] }')
+CC=$(curl -s -X GET http://localhost:4000/chaincode/getAccountBalance -H "content-type: application/json" -H "authorization: Bearer $JWT" -d '{ "ccversion": "v0", "args": ["2ks89j9","importer"] }')
 
-echo ${entity}
-echo
 
 JWT=$(echo $CC | jq '.success')
 if [ $JWT == true ]
