aaa8f43d262ec57f39d7f2f43e915717e035b2d2
darksun113 <mtion@msn.com>
2021-01-15 12:15:28 +0800

    uuid

--- application/client_scripts/getUUID.sh
+++ application/client_scripts/getUUID.sh
@@ -8,7 +8,7 @@ if [ ${JWT:$JLEN:1} == "\"" ]
 then
 	JWT=${JWT:0:$JLEN}
 fi
-CC=$(curl http://localhost:4000/uuid -H "content-type: application/json" -H "authorization: Bearer $JWT")
+CC=$(curl -s -X GET http://localhost:4000/uuid -H "content-type: application/json" -H "authorization: Bearer $JWT")
 JWT=$(echo $CC | jq '.success')
 if [ $JWT == true ]
 then
