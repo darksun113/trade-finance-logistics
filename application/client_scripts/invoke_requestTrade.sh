7615c5e30702f65e276bce161595d8ef30e618fe
darksun113 <mtion@msn.com>
2021-01-15 13:34:01 +0800

    trade

--- application/client_scripts/invoke_requestTrade.sh
+++ application/client_scripts/invoke_requestTrade.sh
@@ -1,3 +1,10 @@
+if [ $# -gt 0 ]
+then
+    tradeid=$1
+else
+    tradeid="2ks89j9"
+fi
+
 JWT=$(jq '.token' userCred.json)
 if [ ${JWT:0:1} == "\"" ]
 then
@@ -8,7 +15,7 @@ if [ ${JWT:$JLEN:1} == "\"" ]
 then
 	JWT=${JWT:0:$JLEN}
 fi
-CC=$(curl -s -X POST http://localhost:4000/chaincode/requestTrade -H "content-type: application/json" -H "authorization: Bearer $JWT" -d '{ "ccversion": "v0", "args": ["2ks89j9", "50000","Wood for Toys"] }')
+CC=$(curl -s -X POST http://localhost:4000/chaincode/requestTrade -H "content-type: application/json" -H "authorization: Bearer $JWT" -d '{ "ccversion": "v0", "args": ["'"$tradeid"'", "50000","Wood for Toys"] }')
 JWT=$(echo $CC | jq '.success')
 if [ $JWT == true ]
 then
