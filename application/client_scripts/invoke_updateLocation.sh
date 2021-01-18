2a34469111f2c4152d557ec2715da2f29c9bece6
darksun113 <mtion@msn.com>
2021-01-18 10:17:20 +0800

    location

--- application/client_scripts/invoke_updateLocation.sh
+++ application/client_scripts/invoke_updateLocation.sh
@@ -5,6 +5,13 @@ else
     tradeid="2ks89j9"
 fi
 
+if [ $# -eq 2 ]
+then
+    location=$2
+else
+    location="DESTINATION"
+fi
+
 JWT=$(jq '.token' userCred.json)
 if [ ${JWT:0:1} == "\"" ]
 then
@@ -15,7 +22,7 @@ if [ ${JWT:$JLEN:1} == "\"" ]
 then
 	JWT=${JWT:0:$JLEN}
 fi
-CC=$(curl -s -X POST http://localhost:4000/chaincode/updateShipmentLocation -H "content-type: application/json" -H "authorization: Bearer $JWT" -d '{ "ccversion": "v0", "args": ["'"$tradeid"'", "Pacific Oceans"] }')
+CC=$(curl -s -X POST http://localhost:4000/chaincode/updateShipmentLocation -H "content-type: application/json" -H "authorization: Bearer $JWT" -d '{ "ccversion": "v0", "args": ["'"$tradeid"'", "'"$location"'"] }')
 JWT=$(echo $CC | jq '.success')
 if [ $JWT == true ]
 then
